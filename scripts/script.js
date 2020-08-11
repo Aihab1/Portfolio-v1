cf6e45c8abceba3718560012e275ee1dc5534f07
Aihab1 <aihab.umair.che19@itbhu.ac.in>
2020-08-12 00:16:40 +0530

    Add intro + designs

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+var last_scroll_top = 0;
+$(window).on('scroll', function () {
+    scroll_top = $(this).scrollTop();
+    if (scroll_top < last_scroll_top) {
+        $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+    }
+    else {
+        $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+    }
+    last_scroll_top = scroll_top;
+
+    if(scroll_top == 0){
+        document.querySelector('.smart-scroll').style.boxShadow = "none";
+    }
+    else{
+        document.querySelector('.smart-scroll').style.boxShadow = "0px 0px 15px rgb(1, 21, 20)";
+    }
+});
