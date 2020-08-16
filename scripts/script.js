ad21de61f7d3495e5f299f6abfd1280123d84e60
Aihab1 <aihab.umair.che19@itbhu.ac.in>
2020-08-16 22:57:31 +0530

    Completed design

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,39 @@
 var last_scroll_top = 0;
+var navbar_open = false;
 document.addEventListener('scroll', function () {
-    scroll_top = $(this).scrollTop();
-    if (scroll_top < last_scroll_top) {
-        $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
-    }
-    else {
-        $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
-    }
-    last_scroll_top = scroll_top;
+    if (!navbar_open) {
+        scroll_top = $(this).scrollTop();
+        if (scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
 
-    if(scroll_top == 0){
-        document.querySelector('.smart-scroll').style.boxShadow = "none";
-    }
-    else{
-        document.querySelector('.smart-scroll').style.boxShadow = "0px 0px 15px rgb(1, 21, 20)";
+        if (scroll_top == 0) {
+            document.querySelector('.smart-scroll').style.boxShadow = "none";
+        }
+        else {
+            document.querySelector('.smart-scroll').style.boxShadow = "0px 0px 15px rgb(1, 21, 20)";
+        }
     }
 });
+
+const navbar = document.querySelector('.hamburger-outer');
+const nav_window = document.querySelector('#links-only');
+const nav_links = document.querySelectorAll('.nav-link');
+const outer_main = document.querySelector('#outer-main');
+
+navbar.addEventListener("click", () => {
+    nav_window.classList.toggle('clicked');
+    navbar.classList.toggle('clicked');
+    outer_main.classList.toggle('clicked');
+    navbar_open = !navbar_open;
+});
+nav_links.forEach(a => a.addEventListener("click", () => {
+    nav_window.classList.toggle('clicked');
+    navbar.classList.toggle('clicked');
+    outer_main.classList.toggle('clicked');
+    navbar_open = !navbar_open;
+}));
