9d68fa8f49247af8c945cbf95736976e02324dee
Aihab1 <aihab.umair.che19@itbhu.ac.in>
2020-08-17 10:09:47 +0530

    Fixed navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,27 @@
 var last_scroll_top = 0;
 var navbar_open = false;
+
 document.addEventListener('scroll', function () {
-    if (!navbar_open) {
+    if (window.innerWidth <= 600) {
+        if (!navbar_open) {
+            scroll_top = $(this).scrollTop();
+            if (scroll_top < last_scroll_top) {
+                $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+            }
+            else {
+                $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+
+            if (scroll_top == 0) {
+                document.querySelector('.smart-scroll').style.boxShadow = "none";
+            }
+            else {
+                document.querySelector('.smart-scroll').style.boxShadow = "0px 0px 15px rgb(1, 21, 20)";
+            }
+        }
+    }
+    else {
         scroll_top = $(this).scrollTop();
         if (scroll_top < last_scroll_top) {
             $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
